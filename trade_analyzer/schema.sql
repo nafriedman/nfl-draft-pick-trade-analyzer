675cf184914c18b9e60b5dd2642f9f2efa3dee0e
Nadav Friedman <nadav.friedman@mail.mcgill.ca>
2024-08-29 16:24:18 -0400

    made change to table

--- trade_analyzer/schema.sql
+++ trade_analyzer/schema.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS jimmy_johnson_chart;
 
 CREATE TABLE jimmy_johnson_chart (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    pick_number INTEGER NOT NULL,
+    pick_number INTEGER PRIMARY KEY,
     value INTEGER NOT NULL
 );
